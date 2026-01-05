b1eaa7441586f87ed37d211417ea4bf18a403ed4
AngelPortilla1 <Angelportihernan019@gmail.com>
2026-01-04 22:57:47 -0500

    Bloque Try-escept iomplementado

--- eliminador_fondos.py
+++ eliminador_fondos.py
@@ -1,25 +1,45 @@
 
 from pathlib import Path
 from rembg import remove
+import datetime
 
 
 class BackgroundRemover:
     
     #Variables de clase
-    SUPPORTED_EXTENSIONS = ('.png','jpg','.jpeg',)
+    SUPPORTED_EXTENSIONS = ('.png','jpg','.jpeg','bmp')
     
     def __init__(self, input_folder, output_folder):
         #Variables de instancia
         self.input_folder = input_folder
         self.output_folder = output_folder
         
-    def process_images(self, filename_list,process_callback=None):
-        pass
+    def process_images(self, filename_list,process_callback=None): #Output:/2026-12-01
+        today_date = datetime.datetime.now().strftime('%Y-%m-%d_H-%M-%S')
+        self._processed_folder= self.output_folder / today_date
+        self._processed_folder.mkdir(parents = True, exist_ok = True)
+        
+        total_files = len(filename_list)
+        processed = 0
+        
+        for filename in filename_list: 
+            if self._is_supported_image(filename):
+                input_path = self.input_folder / filename #descargas/imagen1.png
+                output_path = self._processed_folder / filename
+                
+                try : 
+                    self._remove_background(input_path, output_path)
+                    self._move_original(input_path)
+                except:
+                    print("Hay un error")
+                    
+            
+        
     
     
     def _is_supported_image(self, filename:str):
         
-        return filename.lower().endswith.(self.SUPPORTED_EXTENSIONS)
+        return filename.lower().endswith(self.SUPPORTED_EXTENSIONS)
         
     
     def _remove_background(self, input_path, output_path):
